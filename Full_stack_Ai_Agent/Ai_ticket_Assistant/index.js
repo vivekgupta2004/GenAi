da274f8c2fd3357dd571d9c82347b259c586ed6b
vivekgupta2004 <130595295+vivekgupta2004@users.noreply.github.com>
2025-08-16 17:43:00 +0530

    frontend

--- Full_stack_Ai_Agent/Ai_ticket_Assistant/index.js
+++ Full_stack_Ai_Agent/Ai_ticket_Assistant/index.js
@@ -15,7 +15,12 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+ app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+); 
 
 
 app.use("/api/auth", userRoutes)
